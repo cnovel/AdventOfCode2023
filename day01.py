f0921f2db2af8c178e9fb47e938eb0f578ce0ebb
Cyril NOVEL <5690282+cnovel@users.noreply.github.com>
2023-12-02 08:05:03 +0100

    Day 1

---
+++ day01.py
@@ -0,0 +1,31 @@
+digits = "0123456789"
+
+sum = 0
+with open("data/input_01.txt", 'r') as d:
+   for l in d.readlines():
+      line_digits = [x for x in l if x in digits]
+      number_str = line_digits[0] + line_digits[-1]
+      number = int(number_str)
+      sum += number
+print("Day 01 - Star 1:", sum)
+
+sum = 0
+digits_word_to_digits = {"zero": "0", "one": "1", "two": "2", "three": "3",
+                         "four": "4", "five": "5", "six": "6", "seven": "7",
+                         "eight": "8", "nine": "9"}
+with open("data/input_01.txt", 'r') as d:
+   for l in d.readlines():
+      line_digits = []
+      for i in range(0, len(l)):
+         line = l[i:len(l)]
+         if line[0] in digits:
+            line_digits.append(line[0])
+         for k in digits_word_to_digits.keys():
+            if line.startswith(k):
+               line_digits.append(digits_word_to_digits[k])
+
+      number_str = line_digits[0] + line_digits[-1]
+      number = int(number_str)
+      sum += number
+print("Day 01 - Star 2:", sum)
+
