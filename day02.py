9dab3361685ddefba06214fe7d18fda2b03c5ad5
Cyril NOVEL <5690282+cnovel@users.noreply.github.com>
2023-12-02 08:55:35 +0100

    Better use of regex

--- day02.py
+++ day02.py
@@ -7,30 +7,31 @@ max_red = 12
 max_green = 13
 max_blue = 14
 
-rg_red = re.compile("[0-9]+ red")
-rg_green = re.compile("[0-9]+ green")
-rg_blue = re.compile("[0-9]+ blue")
+rg_game_id = re.compile("Game ([0-9]+):")
+rg_red = re.compile("([0-9]+) red")
+rg_green = re.compile("([0-9]+) green")
+rg_blue = re.compile("([0-9]+) blue")
 
 with open("data/input_02.txt", 'r') as d:
    for l in d.readlines():
-      game_id = int(l.split(":")[0][5:])
+      game_id = int(rg_game_id.findall(l)[0])
       draws = [d.strip() for d in l.split(":")[1].strip().split(";")]
       possible = True
       red = 0
       green = 0
       blue = 0
       for draw in draws:
-         m = [int(s.split(" ")[0]) for s in rg_red.findall(draw)]
+         m = [int(s) for s in rg_red.findall(draw)]
          if m and m[0] > max_red:
             possible = False
          if m:
             red = max(red, m[0])
-         m = [int(s.split(" ")[0]) for s in rg_green.findall(draw)]
+         m = [int(s) for s in rg_green.findall(draw)]
          if m and m[0] > max_green:
             possible = False
          if m:
             green = max(green, m[0])
-         m = [int(s.split(" ")[0]) for s in rg_blue.findall(draw)]
+         m = [int(s) for s in rg_blue.findall(draw)]
          if m and m[0] > max_blue:
             possible = False
          if m:
