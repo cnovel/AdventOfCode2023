9209cb29852b40b7a8b33e15f198a46eddebc100
Cyril NOVEL <5690282+cnovel@users.noreply.github.com>
2023-12-02 11:51:18 +0100

    Use regex for day1

--- day01.py
+++ day01.py
@@ -1,35 +1,34 @@
+import regex as re
+
 from day_processing import Day
 
 class Day01(Day):
    def _process(self):
-      digits = "0123456789"
+      r_a = re.compile("([0-9])")
+      r_b = re.compile("(one|two|three|four|five|six|seven|eight|nine|zero|[0-9])")
 
-      sum = 0
-      for l in self.lines:
-         line_digits = [x for x in l if x in digits]
-         number_str = line_digits[0] + line_digits[-1]
-         number = int(number_str)
-         sum += number
-      print("Day 01 - Star 1:", sum)
-
-      sum = 0
-      digits_word_to_digits = {"zero": "0", "one": "1", "two": "2", "three": "3",
+      transfo = {"zero": "0", "one": "1", "two": "2", "three": "3",
                               "four": "4", "five": "5", "six": "6", "seven": "7",
-                              "eight": "8", "nine": "9"}
-      for l in self.lines:
-         line_digits = []
-         for i in range(0, len(l)):
-            line = l[i:len(l)]
-            if line[0] in digits:
-               line_digits.append(line[0])
-            word = [digits_word_to_digits[k] for k in digits_word_to_digits.keys() if line.startswith(k)]
-            if word:
-               line_digits.append(word[0])
+                              "eight": "8", "nine": "9", "1": "1", "2": "2", "3": "3",
+                              "4": "4", "5": "5", "6": "6", "7": "7", "8": "8",
+                              "9": "9", "0": "0"}
 
-         number_str = line_digits[0] + line_digits[-1]
-         number = int(number_str)
-         sum += number
-      print("Day 01 - Star 2:", sum)
+      sum_a = 0
+      sum_b = 0
+
+      for l in self.lines:
+         matches = r_a.findall(l)
+         nb_str = transfo[matches[0]] + transfo[matches[-1]]
+         nb = int(nb_str)
+         sum_a += nb
+
+         matches = r_b.findall(l.strip(), overlapped=True)
+         nb_str = transfo[matches[0]] + transfo[matches[-1]]
+         nb = int(nb_str)
+         sum_b += nb
+
+      print("Day 01 - Star 1:", sum_a)
+      print("Day 01 - Star 2:", sum_b)
 
 
 if __name__ == "__main__":
