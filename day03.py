3d5f04feced0f2f1e6a70d4d4bfe531081a21e65
Cyril NOVEL <5690282+cnovel@users.noreply.github.com>
2023-12-03 17:10:33 +0100

    Better code

--- day03.py
+++ day03.py
@@ -11,6 +11,7 @@ class BlockNumber:
    def next_to(self, star) -> bool:
       return self.y - 1 <= star[1] <= self.y + 1 and self.x_s - 1 <= star[0] <= self.x_e
 
+
 class Day03(Day):
    def _name(self):
       return "--- Day 3: Gear Ratios ---"
@@ -21,11 +22,14 @@ class Day03(Day):
    def _process(self):
       sum_a = 0
       symbol_pos = set()
+      star_pos = set()
       symbol_rg = re.compile("([^\d.\n])")
       for i in range(0, len(self.lines)):
          l = self.lines[i]
          for s in symbol_rg.finditer(l):
             symbol_pos.add((s.span()[0],i))
+            if s.group() == "*":
+               star_pos.add((s.span()[0],i))
 
       numbers = re.compile("(\d+)")
       number_blocks = []
@@ -36,20 +40,16 @@ class Day03(Day):
             x_e = n.span()[1]
             y_s = i - 1
             y_e = i + 2
-            nb_hits = len([(x,y) for x in range(x_s - 1, x_e + 1) for y in range(y_s, y_e) if (x,y) in symbol_pos])
-            if nb_hits > 0:
+            hit = any([(x,y) in symbol_pos for y in range(y_s, y_e) for x in range(x_s - 1, x_e + 1)])
+            if hit:
                sum_a += int(n.group())
             number_blocks.append(BlockNumber(x_s, x_e, i, int(n.group())))
 
       sum_b = 0
-      star_rg = re.compile("(\*)")
-      for i in range(0, len(self.lines)):
-         l = self.lines[i]
-         for s in star_rg.finditer(l):
-            s_pos = (s.span()[0], i)
-            neighs = [n.value for n in number_blocks if n.next_to(s_pos)]
-            if len(neighs) == 2:
-               sum_b += neighs[0] * neighs[1]
+      for star in star_pos:
+         neighs = [n.value for n in number_blocks if n.next_to(star)]
+         if len(neighs) == 2:
+            sum_b += neighs[0] * neighs[1]
 
       print("Day 03 - Star 1:", sum_a)
       print("Day 03 - Star 2:", sum_b)
