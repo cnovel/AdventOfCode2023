8349ba5970d98946256f57871ed58515242e947c
Cyril NOVEL <5690282+cnovel@users.noreply.github.com>
2023-12-18 15:22:22 +0100

    Smarter computation!
    Change-Id: Ic4202210a1eadcb8fb5bbf12a7ad9fc7d8c47622

--- day18.py
+++ day18.py
@@ -115,12 +115,64 @@ class Day18(Day):
                 area += dx * dy
         return area
 
+    @staticmethod
+    def get_area_smart(instructions):
+        pos = (0, 0)
+        positions = [pos]
+        for i in instructions:
+            direction = get_dir(i[0])
+            n = int(i[1])
+            new_pos = (pos[0] + n * direction[0], pos[1] + n * direction[1])
+            pos = new_pos
+            positions.append(pos)
+
+        positions = positions[:-1]
+        real_pos = []
+        for c in range(len(positions)):
+            p = (c - 1) % len(positions)
+            n = (c + 1) % len(positions)
+
+            pos_c = positions[c]
+            pos_p = positions[p]
+            pos_n = positions[n]
+
+            # Clockwise
+            if pos_p[0] == pos_c[0] and pos_p[1] > pos_c[1] and pos_n[0] > pos_c[0] and pos_n[1] == pos_c[1]:
+                real_pos.append(pos_c)
+            elif pos_p[0] < pos_c[0] and pos_p[1] == pos_c[1] and pos_n[0] == pos_c[0] and pos_n[1] > pos_c[1]:
+                real_pos.append((pos_c[0] + 1, pos_c[1]))
+            elif pos_p[0] == pos_c[0] and pos_p[1] < pos_c[1] and pos_n[0] < pos_c[0] and pos_n[1] == pos_c[1]:
+                real_pos.append((pos_c[0] + 1, pos_c[1] + 1))
+            elif pos_p[0] > pos_c[0] and pos_p[1] == pos_c[1] and pos_n[0] == pos_c[0] and pos_n[1] < pos_c[1]:
+                real_pos.append((pos_c[0], pos_c[1] + 1))
+            # Counterclockwise
+            elif pos_p[0] > pos_c[0] and pos_p[1] == pos_c[1] and pos_n[0] == pos_c[0] and pos_n[1] > pos_c[1]:
+                real_pos.append((pos_c[0] + 1, pos_c[1] + 1))
+            elif pos_p[0] == pos_c[0] and pos_p[1] < pos_c[1] and pos_n[0] > pos_c[0] and pos_n[1] == pos_c[1]:
+                real_pos.append((pos_c[0] + 1, pos_c[1]))
+            elif pos_p[0] < pos_c[0] and pos_p[1] == pos_c[1] and pos_n[0] == pos_c[0] and pos_n[1] < pos_c[1]:
+                real_pos.append(pos_c)
+            elif pos_p[0] == pos_c[0] and pos_p[1] > pos_c[1] and pos_n[0] < pos_c[0] and pos_n[1] == pos_c[1]:
+                real_pos.append((pos_c[0], pos_c[1] + 1))
+
+        seg_hor = []
+        for i in range(len(real_pos)):
+            p = real_pos[i]
+            np = real_pos[(i + 1) % len(real_pos)]
+            if p[0] != np[0]:
+                seg_hor.append((p, np))
+
+        area = 0
+        for s in seg_hor:
+            area += (s[0][0] - s[1][0]) * s[0][1]
+        return area
+
     def _process(self):
         rg = re.compile(r"([RDLU]) (\d+) \(#(.+)\)")
         instructions = [rg.findall(line)[0] for line in self.lines]
-        print("Day 18 - Star 1:", self.get_area(instructions))
-        print("Day 18 - Star 2:", self.get_area([(get_letter(int(i[2][-1])), int(i[2][:5], 16))
-                                                 for i in instructions]))
+        print("Day 18 - Star 1:", self.get_area_smart(instructions))
+        print("Day 18 - Star 2:", self.get_area_smart([(get_letter(int(i[2][-1])), int(i[2][:5], 16))
+                                                       for i in instructions]))
 
 
 if __name__ == "__main__":
