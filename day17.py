8177267b679a94239209db4eece196f705c9706b
Cyril NOVEL <5690282+cnovel@users.noreply.github.com>
2023-12-20 21:39:12 +0100

    Day 17 lint
    Change-Id: Icdf43e4b87c6298afd831281a388eb5c297599e5

--- day17.py
+++ day17.py
@@ -6,6 +6,7 @@ EAST = (0, 1)
 SOUTH = (1, 0)
 WEST = (0, -1)
 
+
 class Day17(Day):
     def _name(self):
         return "--- Day 17: Clumsy Crucible ---"
@@ -23,28 +24,28 @@ class Day17(Day):
 
         h = len(self.lines)
         w = len(self.lines[-1])
-        (y, x, dir, nb_moves) = curr
+        (y, x, direction, nb_moves) = curr
         neigh = []
         for new_dir in [EAST, WEST, NORTH, SOUTH]:
             (new_y, new_x) = (y + new_dir[0], x + new_dir[1])
-            if not (new_y >= 0 and new_x >= 0 and new_y < h and new_x < w):
+            if not (0 <= new_y < h and 0 <= new_x < w):
                 continue
 
-            if dir == new_dir:
+            if direction == new_dir:
                 new_direction_moves = nb_moves + 1
             else:
                 new_direction_moves = 1
 
             # Max move condition
-            if new_direction_moves > max_moves  :
+            if new_direction_moves > max_moves:
                 continue
 
             # Min move condition before turning if ultra
-            if min_moves and new_dir != dir and nb_moves < min_moves:
+            if min_moves and new_dir != direction and nb_moves < min_moves:
                 continue
 
             # No reversing
-            if (new_dir[0] * -1, new_dir[1] * -1) == dir:
+            if (new_dir[0] * -1, new_dir[1] * -1) == direction:
                 continue
 
             neigh.append((new_y, new_x, new_dir, new_direction_moves))
@@ -65,11 +66,12 @@ class Day17(Day):
             (cost, curr) = heappop(q)
             if curr in dist:  # We already know the optimal cost for this position/direction/moves
                 continue
-            dist[curr] = cost # Save optimal cost
+            dist[curr] = cost  # Save optimal cost
 
             for n in self.get_neigh(curr, ultra):
                 new_cost = cost + int(self.lines[n[0]][n[1]])
-                if n not in dist or new_cost < dist[curr]:  # Either we have not yet encounter n or we found a best way to encounter it
+                # Either we have not yet encounter n or we found a better way to encounter it
+                if n not in dist or new_cost < dist[curr]:
                     heappush(q, (new_cost, n))
 
         return min([c for ((y, x, _dir, _moves), c) in dist.items() if (x, y) == end])
