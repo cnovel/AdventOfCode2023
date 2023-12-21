03c7c27a12954ac54a5080bd5860b75f2a5b565d
Cyril NOVEL <5690282+cnovel@users.noreply.github.com>
2023-12-21 15:42:37 +0100

    Day 21.2
    Change-Id: Ib994c61c621666862901c75bf121d29726cb78e2

--- day21.py
+++ day21.py
@@ -14,31 +14,34 @@ class Day21(Day):
     def _file(self):
         return "data/input_21.txt"
 
-    def _get_neigh(self, pos, h, w):
+    def _get_neigh(self, pos, h, w, max_cost):
         n = []
         for d in [NORTH, EAST, SOUTH, WEST]:
             new_pos = (pos[0] + d[0], pos[1] + d[1])
-            if not (0 <= new_pos[0] < w and 0 <= new_pos[1] < h):
+            if not max_cost and not (0 <= new_pos[0] < w and 0 <= new_pos[1] < h):
                 continue
-            if self.lines[new_pos[1]][new_pos[0]] == "#":
+            e = (new_pos[0] % w, new_pos[1] % h)
+            if self.lines[e[1]][e[0]] == "#":
                 continue
             n.append(new_pos)
         return n
 
-    def _dijkstra(self, start, start_cost):
+    def _dijkstra(self, start, max_cost=None):
         h = len(self.lines)
         w = len(self.lines[-1])
         dist = {}
         q = []
         # In q, (cost, (x,y))
-        heappush(q, (start_cost, start))
+        heappush(q, (0, start))
         while q:
             (cost, pos) = heappop(q)
             if pos in dist:
                 continue
             dist[pos] = cost
-            for n in self._get_neigh(pos, h, w):
+            for n in self._get_neigh(pos, h, w, max_cost):
                 new_cost = cost + 1
+                if max_cost and new_cost > max_cost:
+                    continue
                 if n not in dist or new_cost < dist[n]:
                     heappush(q, (new_cost, n))
 
@@ -56,8 +59,28 @@ class Day21(Day):
 
         moves = 64
         r = moves % 2
-        dist = self._dijkstra(start, 0)
+        dist = self._dijkstra(start, None)
         self.prnt_a(len([x for x in dist.values() if x <= moves and x % 2 == r]))
+
+        # It actually fits a quadratic equation, thanks Reddit
+        moves = 26501365
+        seen_states = []
+        mod = moves % h
+        n = moves // h
+        # We need the first 3 terms to get the equation
+        for run in [mod, mod + h, mod + h * 2]:
+            r = run % 2
+            dist = self._dijkstra(start, run)
+            seen_states.append(len([x for x in dist.values() if x <= moves and x % 2 == r]))
+
+        y0 = seen_states[0]
+        y1 = seen_states[1]
+        y2 = seen_states[2]
+        a = (y2 + y0 - 2 * y1) / 2
+        b = y1 - y0 - a
+        c = y0
+
+        self.prnt_b(int(a*n**2 + b*n + c))
 
 
 if __name__ == "__main__":
