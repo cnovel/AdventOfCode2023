f0fe1d5b8405ec6ffbdf85daeb8226dc28427d1f
Cyril NOVEL <5690282+cnovel@users.noreply.github.com>
2023-12-29 10:24:02 +0100

    Day 24 gold

--- day24.py
+++ day24.py
@@ -1,5 +1,6 @@
 import re
 from day_processing import Day
+import collections
 
 
 class HailLine2D:
@@ -21,6 +22,39 @@ class HailLine2D:
     def is_future(self, xy):
         v_n = (xy[0] - self.sp[0], xy[1] - self.sp[1])
         return v_n[0]*self.v[0] + v_n[1]*self.v[1] > 0
+
+
+Vec3 = collections.namedtuple("Vec3", "x,y,z", defaults = (0, 0, 0))
+
+
+def cross(a, b):
+    return Vec3(a.y*b.z - a.z*b.y, a.z*b.x - a.x*b.z, a.x*b.y - a.y*b.x)
+
+
+def colinear(a, b) -> bool:
+    return all(v == 0 for v in cross(a, b))
+
+
+def dot(a, b):
+    return a.x*b.x + a.y*b.y + a.z*b.z
+
+
+def sub(a, b):
+    return Vec3(a.x-b.x, a.y-b.y, a.z-b.z)
+
+
+def get_plane(p1, v1, p2, v2):
+    p12 = sub(p1, p2)
+    v12 = sub(v1, v2)
+    vv = cross(v1, v2)
+    return (cross(p12, v12), dot(p12, vv))
+
+
+def lin(r, a, s, b, t, c):
+    x = r*a.x + s*b.x + t*c.x
+    y = r*a.y + s*b.y + t*c.y
+    z = r*a.z + s*b.z + t*c.z
+    return Vec3(x, y, z)
 
 
 class Day24(Day):
@@ -51,6 +85,44 @@ class Day24(Day):
                 if hj.is_future(xy) and hi.is_future(xy):
                     star_a += 1
         self.prnt_a(star_a)
+
+        points = []
+        speeds = []
+        for line in self.lines:
+            m = [int(c) for c in rg_line.findall(line)[0]]
+            points.append(Vec3(m[0], m[1], m[2]))
+            speeds.append(Vec3(m[3], m[4], m[5]))
+
+        n = len(points)
+        p1, v1 = points[0], speeds[0]
+        for i in range(1, n):
+            if not colinear(v1, speeds[i]):
+                p2, v2 = points[i], speeds[i]
+                break
+        for j in range(i+1, n):
+            if not colinear(v1, speeds[j]) and not colinear(v2, speeds[j]):
+                p3, v3 = points[j], speeds[j]
+
+        a, A = get_plane(p1, v1, p2, v2)
+        b, B = get_plane(p1, v1, p3, v3)
+        c, C = get_plane(p2, v2, p3, v3)
+
+        w = lin(A, cross(b, c), B, cross(c, a), C, cross(a, b))
+        t = dot(a, cross(b, c))
+        w = Vec3(round(w.x / t), round(w.y / t), round(w.z / t))
+
+        w1 = sub(v1, w)
+        w2 = sub(v2, w)
+        ww = cross(w1, w2)
+
+        E = dot(ww, cross(p2, w2))
+        F = dot(ww, cross(p1, w1))
+        G = dot(p1, ww)
+        S = dot(ww, ww)
+
+        rock = lin(E, w1, -F, w2, G, ww)
+        self.prnt_b(int(sum(rock) / S))
+
 
 
 if __name__ == "__main__":
